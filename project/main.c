2622d920868b6fa9a5f4d2f3515ca7f8ace19e5b
jherr029 <jherr029@ucr.edu>
2017-11-30 16:22:50 -0800

    first commit

---
+++ project/main.c
@@ -0,0 +1,18 @@
+/*
+ * project.c
+ *
+ * Created: 11/30/2017 4:11:58 PM
+ * Author : IEUser
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
